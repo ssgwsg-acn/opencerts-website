d81ce91a1004d95ceed6b6fecb2b8014fd6ccff2
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-03-29 14:48:20 +0800

    -  Minimal cert added
    - added a function to add data field in minimal cert

--- src/sagas/templates.test.js
+++ src/sagas/templates.test.js
@@ -2,22 +2,27 @@ import { put, call } from "redux-saga/effects";
 import { get } from "lodash";
 import { certificateData } from "@govtechsg/open-certificate";
 import templates from "../components/CertificateTemplates";
-import json from "../../tests_fixture/NP_Certs_FT_BMS.json";
 import { preloadTemplateChunk } from "./templates";
 import { types as templateActions } from "../reducers/templates";
+import { MakeCertUtil } from "./testutils";
 
 describe("preload template", () => {
-  it("should fetch the template name", () => {
-    expect(get(certificateData(json), "$template", "default")).toEqual(
-      "NP-AA2018-BMS(CLT)"
+  const template = "97d60572-7694-4d91-a9a1-f05771d51a8f:string:NP-AA2018-DPP";
+  const testCert = new MakeCertUtil()
+    .addDataField("$template", template)
+    .finish();
+
+  it("Should fetch the template name", () => {
+    expect(get(certificateData(testCert), "$template", "default")).toEqual(
+      "NP-AA2018-DPP"
     );
   });
 
-  const loadingTemplate = "NP-AA2018-BMS(CLT)";
+  const loadingTemplate = "NP-AA2018-DPP";
 
-  const saga = preloadTemplateChunk({ payload: json });
+  const saga = preloadTemplateChunk({ payload: testCert });
 
-  it("should prefetch the template chunk", () => {
+  it("Should prefetch the template chunk", () => {
     expect(saga.next().value).toEqual(
       put({
         type: templateActions.PRELOAD_TEMPLATE_CHUNK,
